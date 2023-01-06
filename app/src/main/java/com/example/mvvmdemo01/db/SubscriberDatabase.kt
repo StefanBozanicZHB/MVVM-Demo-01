ef2b1198cffddddd4b5edd44ba849bc6e46e345b
Stefan Bozanic <zhb.beograd@gmail.com>
2023-01-06 09:52:02 +0100

    SubscriberDatabase

---
+++ app/src/main/java/com/example/mvvmdemo01/db/SubscriberDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.mvvmdemo01.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase : RoomDatabase() {
+
+    abstract val subscriberDAO: SubscriberDAO
+
+    companion object {
+        private const val databaseName = "subscriber_data_database"
+
+        @Volatile
+        private var INSTANCE: SubscriberDatabase? = null
+        fun getInstance(context: Context): SubscriberDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDatabase::class.java,
+                        databaseName,
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
