ba26b81759aed88eca418a66b01a89ed9eebbc6a
Stefan Bozanic <zhb.beograd@gmail.com>
2023-01-07 11:40:08 +0100

    Refactoring code

--- app/src/main/java/com/example/mvvmdemo01/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdemo01/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.mvvmdemo01.db.SubscriberRepository
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var subscriberViewModel: SubscriberViewModel
+    private lateinit var adapter: MyRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,15 +37,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun initRecyclerView() {
         binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter =
+            MyRecyclerViewAdapter { selectedItem: Subscriber -> listItemClicked(selectedItem) }
+        binding.subscriberRecyclerView.adapter = adapter
         displaySubscribersList()
 
     }
 
     private fun displaySubscribersList() {
         subscriberViewModel.subscribers.observe(this) {
-            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(
-                it
-            ) { selectedItem: Subscriber -> listItemClicked(selectedItem) }
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
         }
     }
 
